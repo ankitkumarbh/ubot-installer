04cc7fda42d28a7a9ab3c0136dfcd0f74047ca5c
Ankit Choudhary <ankitkumarbh@gmail.com>
2024-05-26 17:03:48 +0530

    first commit

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+filepath="$(realpath $0)"
+mypath="$(dirname "$filepath")"
+cd $mypath
+
+git clone $REPO_URL BOT
+
+git clone $REPO_URL2 BOT2
+
+bash BOT/start.sh & bash BOT2/start.sh & wait -n
